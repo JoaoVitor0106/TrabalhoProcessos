081155d2b56fda5c501aa7de2a9cb255edcff65e
João Vitor <joaovitor.teixeira@a.ucb.br>
2025-04-09 20:20:37 -0300

    att 4
    bugs resolvidos, todas as funções estão funcionando

--- processo.h
+++ processo.h
@@ -1,19 +1,25 @@
-// processo.h
+//processos.h
 #ifndef PROCESSO_H
 #define PROCESSO_H
 
 typedef struct {
-    long id;    //Identificador do processo
-    char numero[30];    //Número do processo
-    char data_ajuizamento[25];  // Data do Ajuizamento
-    int id_classe;  //ID da classe
-    int* id_assuntos;     //Array de id Assuntos (pode ser multiplo)
-    int num_assuntos;   //Número de assuntos associados ao processo
-    int ano_eleicao;    //Ano de eleição
+    long id;
+    char numero[30];
+    char data_ajuizamento[25];
+    int id_classe;
+    int* id_assuntos;
+    int num_assuntos;
+    int ano_eleicao;
 } Processo;
 
+int carregar_processos(Processo *processos, const char *nome_arquivo);
 int contarPorClasse(Processo *processos, int tamanho, int id_classe);
 int contar_assuntos_unicos(Processo *processos, int n);
 void listar_processos_multiplos_assuntos(Processo *processos, int n);
+void remover_chaves_aspas(char *str);
+
+// Novas funções do menu
+void opcao_contar_por_classe(Processo *processos, int total);
+void opcao_contar_assuntos_unicos(Processo *processos, int total);
 
 #endif
